6d472c2bdee1b16c5353ba62c7bf9b5aa22ac063
ericlindstrom <eric.lindstrom@gmail.com>
2012-08-25 11:56:29 -0700

    cleanup

--- blog/urls.py
+++ blog/urls.py
@@ -2,16 +2,16 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^$', index_view, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     # tags
-    url(r'^tags/$', tag_list, name='tag_list'),
-    url(r'^tag/(?P<slug>.*)/$', tag_detail, name='tag_detail'),
+    url(r'^tags/$', TagListView.as_view(), name='tag_list'),
+    url(r'^tag/(?P<slug>.*)/$', TagDetailView.as_view(), name='tag_detail'),
     # categories
-    url(r'^categories/$', category_list, name='category_list'),
-    url(r'^category/(?P<slug>.*)/$', category_detail, name='category_detail'),
+    url(r'^categories/$', CategoryListView.as_view(), name='category_list'),
+    url(r'^category/(?P<slug>.*)/$', CategoryDetailView.as_view(), name='category_detail'),
     # archive
-    url(r'^(?P<year>\d{4})/$', entry_year_detail, name='entry_year_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', entry_month_detail, name='entry_month_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', entry_day_detail, name='entry_day_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>.*)/$', entry_detail, name='entry_detail'),
+    url(r'^(?P<year>\d{4})/$', EntryYearView.as_view(), name='entry_year_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', EntryMonthView.as_view(), name='entry_month_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', EntryDayView.as_view(), name='entry_day_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>.*)/$', EntryDetailView.as_view(), name='entry_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -32,18 +32,3 @@ class IndexView(ArchiveIndexView):
     date_field = 'date'
     model = BlogEntry
 
-    def get_context_data(self, *kwargs):
-	context = super(IndexView, self).get_context_data(**kwargs)
-
-	return context
-
-
-index_view = IndexView.as_view()
-tag_list = TagListView.as_view()
-tag_detail = TagDetailView.as_view()
-category_list = CategoryListView.as_view()
-category_detail = CategoryDetailView.as_view()
-entry_year_detail = EntryYearView.as_view()
-entry_month_detail = EntryMonthView.as_view()
-entry_day_detail = EntryDayView.as_view()
-entry_detail = EntryDetailView.as_view()
